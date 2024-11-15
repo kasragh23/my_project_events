bc70cabdd256acb8f3391f2dfc610562f82942bb
kasragh23 <kasrapr23@gmail.com>
2024-11-15 17:52:01 +0330

    price & capacity digitOnly

--- lib/src/pages/add_event/controllers/add_event_controller.dart
+++ lib/src/pages/add_event/controllers/add_event_controller.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import '../model/add_event_dto.dart';
-
 
 import '../repositories/add_event_repository.dart';
 
@@ -12,20 +12,19 @@ class AddEventController extends GetxController {
   final int? userId;
 
   AddEventController({required this.userId});
-
   final formKey = GlobalKey<FormState>();
   final title = TextEditingController();
   final description = TextEditingController();
   final capacity = TextEditingController();
   final price = TextEditingController();
+  final TextInputFormatter _capacityFormatter = FilteringTextInputFormatter.digitsOnly;
+  final TextInputFormatter _priceFormatter = FilteringTextInputFormatter.allow(RegExp('[0-9.,]+'));
   var selectedYear = 0.obs;
   var selectedMonth = 0.obs;
   var selectedDay = 0.obs;
   var selectedHour = 0.obs;
   var selectedMinute = 0.obs;
-  final currentYear = DateTime
-      .now()
-      .year;
+  final currentYear = DateTime.now().year;
 
   List<int> get years =>
       [for (var i = currentYear - 5; i <= currentYear + 5; i++) i];
@@ -50,6 +49,9 @@ class AddEventController extends GetxController {
   var selectedImage = ''.obs;
   var selectedDate = ''.obs;
 
+  List<TextInputFormatter> get capacityFormatters => [_capacityFormatter];
+  List<TextInputFormatter> get priceFormatters => [_priceFormatter];
+
   final ImagePicker _picker = ImagePicker();
 
   void pickImage() async {
@@ -67,55 +69,69 @@ class AddEventController extends GetxController {
   Future<void> addEvent() async {
     if (!(formKey.currentState?.validate() ?? false)) return;
 
-      final int priceValue = int.tryParse(price.text) ?? 0;
-      final int capacityValue = int.parse(capacity.text);
-      final eventDate = DateTime(
-        selectedYear.value, selectedMonth.value, selectedDay.value,
-        selectedHour.value, selectedMinute.value,);
-      if (eventDate.isBefore(DateTime.now())) {
-        Get.showSnackbar(
-          const GetSnackBar(title: 'Error',
-            message: 'Event date must be in the future.',
-            backgroundColor: Colors.red,
-            duration: Duration(seconds: 4),
-          ),);
-        return;
-      }
-        final AddEventDto dto = AddEventDto(
-          userId: userId!,
-          title: title.text,
-          description: description.text,
-          poster: selectedImage.value,
-          date: eventDate.toString(),
-          capacity: capacityValue,
-          price: priceValue,
-        );
-        final resultOrException = await _repository.addEvent(dto: dto);
-        resultOrException.fold((exception) {
-          Get.showSnackbar(
-            GetSnackBar(
-              title: 'Failed to add event',
-              message: exception,
-              backgroundColor: Colors.red,
-              duration: const Duration(seconds: 4),
-            ),
-          );
-        }, (event) {
-            Get.back();
-        });
-
+    final int priceValue = int.tryParse(price.text) ?? 0;
+    final int capacityValue = int.parse(capacity.text);
+    final eventDate = DateTime(
+      selectedYear.value,
+      selectedMonth.value,
+      selectedDay.value,
+      selectedHour.value,
+      selectedMinute.value,
+    );
+    if (eventDate.isBefore(DateTime.now())) {
+      Get.showSnackbar(
+        const GetSnackBar(
+          title: 'Error',
+          message: 'Event date must be in the future.',
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 4),
+        ),
+      );
+      return;
     }
-
-    //   void selectDate(BuildContext context) async {
-    //     final DateTime? picked = await showDatePicker(
-    //       context: context,
-    //       initialDate: DateTime.now(),
-    //       firstDate: DateTime(2000),
-    //       lastDate: DateTime(2101),
-    //     );
-    //     if (picked != null && picked != DateTime.now()) {
-    //       selectedDate.value = picked.toLocal().toString().split(' ')[0];
-    //     }
-    //   }
-    // }
+    final AddEventDto dto = AddEventDto(
+      userId: userId!,
+      title: title.text,
+      description: description.text,
+      poster: selectedImage.value,
+      date: eventDate.toString(),
+      capacity: capacityValue,
+      price: priceValue,
+    );
+    final resultOrException = await _repository.addEvent(dto: dto);
+    resultOrException.fold((exception) {
+      Get.showSnackbar(
+        GetSnackBar(
+          title: 'Failed to add event',
+          message: exception,
+          backgroundColor: Colors.red,
+          duration: const Duration(seconds: 4),
+        ),
+      );
+    }, (event) {
+      Get.back();
+    });
   }
+
+  //   void selectDate(BuildContext context) async {
+  //     final DateTime? picked = await showDatePicker(
+  //       context: context,
+  //       initialDate: DateTime.now(),
+  //       firstDate: DateTime(2000),
+  //       lastDate: DateTime(2101),
+  //     );
+  //     if (picked != null && picked != DateTime.now()) {
+  //       selectedDate.value = picked.toLocal().toString().split(' ')[0];
+  //     }
+  //   }
+  // }
+
+  @override
+  void dispose() {
+    super.dispose();
+    title.dispose();
+    description.dispose();
+    capacity.dispose();
+    price.dispose();
+  }
+}
--- lib/src/pages/add_event/view/add_event_page.dart
+++ lib/src/pages/add_event/view/add_event_page.dart
@@ -21,196 +21,230 @@ class AddEventPage extends GetView<AddEventController> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text(LocaleKeys.localization_app_add_event.tr),
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
-          actions: [
-            TextButton(
-                onPressed: () => Get.updateLocale(const Locale('en', 'US')),
-                child: Text(
-                  LocaleKeys.localization_app_change_language_to_english.tr,
-                  style: const TextStyle(color: Colors.white),
-                )),
-            TextButton(
-              onPressed: () => Get.updateLocale(const Locale('fa', 'IR')),
-              child: Text(
-                LocaleKeys.localization_app_change_language_to_persian.tr,
-                style: const TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: controller.formKey,
-                child: SizedBox(
-                  width: responsiveWidth(context),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: controller.title,
-                        decoration: InputDecoration(
-                            labelText: LocaleKeys.localization_app_title.tr),
-                        validator: controller.validate,
-                      ),
-                      TextFormField(
-                        controller: controller.description,
-                        decoration: InputDecoration(
-                            labelText:
-                                LocaleKeys.localization_app_description.tr),
-                        validator: controller.validate,
-                      ),
-                      const SizedBox(height: 16),
-                      Obx(() => controller.selectedImage.value.isEmpty
-                          ? const CircleAvatar(
-                              child: ClipOval(
-                                  child: Icon(Icons.image_not_supported)))
-                          : CircleAvatar(
-                              child: ClipOval(
-                                child: Image.memory(base64Decode(
-                                    controller.selectedImage.value)),
-                              ),
-                            )),
-                      verticalGap(),
-                      ElevatedButton(
-                        onPressed: controller.pickImage,
-                        child: Text(LocaleKeys.localization_app_pick_image.tr),
-                      ),
-                      const SizedBox(height: 16),
-                      // Obx(() => TextField(
-                      //       controller: TextEditingController(
-                      //           text: controller.selectedDate.value),
-                      //       readOnly: true,
-                      //       decoration: const InputDecoration(labelText: 'Date'),
-                      //       onTap: () => controller.selectDate(context),
-                      //     )),
-                      Row(
-                        children: [
-                          Obx(() => DropdownButton<int>(
-                                hint: Text(LocaleKeys.localization_app_year.tr),
-                                value: controller.selectedYear.value == 0
-                                    ? null
-                                    : controller.selectedYear.value,
-                                items: controller.years
-                                    .map<DropdownMenuItem<int>>(
-                                        (int value) => DropdownMenuItem<int>(
-                                              value: value,
-                                              child: Text(value.toString()),
-                                            ))
-                                    .toList(),
-                                onChanged: (value) {
-                                  controller.selectedYear.value = value!;
-                                },
-                              )),
-                          horizontalGap(),
-                          // Month Dropdown
-                          Obx(() => DropdownButton<int>(
-                                hint:
-                                    Text(LocaleKeys.localization_app_month.tr),
-                                value: controller.selectedMonth.value == 0
-                                    ? null
-                                    : controller.selectedMonth.value,
-                                items: controller.months
-                                    .map<DropdownMenuItem<int>>(
-                                        (int value) => DropdownMenuItem<int>(
-                                              value: value,
-                                              child: Text(value.toString()),
-                                            ))
-                                    .toList(),
-                                onChanged: (value) {
-                                  controller.selectedMonth.value = value!;
-                                  controller.selectedDay.value = 0;
-                                },
-                              )),
-                          horizontalGap(),
-                          // Day Dropdown
-                          Obx(() => DropdownButton<int>(
-                                hint: Text(LocaleKeys.localization_app_day.tr),
-                                value: controller.selectedDay.value == 0
-                                    ? null
-                                    : controller.selectedDay.value,
-                                items: controller.days
-                                    .map<DropdownMenuItem<int>>(
-                                        (int value) => DropdownMenuItem<int>(
-                                              value: value,
-                                              child: Text(value.toString()),
-                                            ))
-                                    .toList(),
-                                onChanged: (value) {
-                                  controller.selectedDay.value = value!;
-                                },
-                              )),
-                        ],
-                      ),
-                      horizontalGap(),
-                      // Hour Dropdown
-                      Row(children: [
-                        Obx(() => DropdownButton<int>(
-                              hint: Text(LocaleKeys.localization_app_hour.tr),
-                              value: controller.selectedHour.value == 0
-                                  ? null
-                                  : controller.selectedHour.value,
-                              items: controller.hours
-                                  .map<DropdownMenuItem<int>>(
-                                      (int value) => DropdownMenuItem<int>(
-                                            value: value,
-                                            child: Text(value.toString()),
-                                          ))
-                                  .toList(),
-                              onChanged: (value) {
-                                controller.selectedHour.value = value!;
-                              },
-                            )),
-                        horizontalGap(),
-                        // Minute Dropdown
-                        Obx(() => DropdownButton<int>(
-                              hint: Text(LocaleKeys.localization_app_minute.tr),
-                              value: controller.selectedMinute.value == 0
-                                  ? null
-                                  : controller.selectedMinute.value,
-                              items: controller.minutes
-                                  .map<DropdownMenuItem<int>>(
-                                      (int value) => DropdownMenuItem<int>(
-                                            value: value,
-                                            child: Text(value.toString()),
-                                          ))
-                                  .toList(),
-                              onChanged: (value) {
-                                controller.selectedMinute.value = value!;
-                              },
-                            )),
-                      ]),
+        appBar: _appBar(),
+        body: _body(context),
+      );
 
-                      TextFormField(
-                        controller: controller.capacity,
-                        decoration: InputDecoration(
-                            labelText: LocaleKeys.localization_app_capacity.tr),
-                        keyboardType: TextInputType.number,
-                        validator: controller.validate,
-                      ),
-                      TextFormField(
-                        controller: controller.price,
-                        decoration: InputDecoration(
-                            labelText: LocaleKeys.localization_app_price.tr),
-                        keyboardType: TextInputType.number,
-                        validator: controller.validate,
-                      ),
-                      const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: controller.addEvent,
-                        child: Text(LocaleKeys.localization_app_add_event.tr),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(LocaleKeys.localization_app_add_event.tr),
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+      actions: [
+        TextButton(
+          onPressed: () => Get.updateLocale(const Locale('en', 'US')),
+          child: Text(
+            LocaleKeys.localization_app_change_language_to_english.tr,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        TextButton(
+          onPressed: () => Get.updateLocale(const Locale('fa', 'IR')),
+          child: Text(
+            LocaleKeys.localization_app_change_language_to_persian.tr,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: controller.formKey,
+            child: SizedBox(
+              width: responsiveWidth(context),
+              child: _textFields(),
             ),
           ),
         ),
-      );
+      ),
+    );
+  }
+
+  Column _textFields() {
+    return Column(
+      children: [
+        TextFormField(
+          controller: controller.title,
+          decoration:
+              InputDecoration(labelText: LocaleKeys.localization_app_title.tr),
+          validator: controller.validate,
+        ),
+        TextFormField(
+          controller: controller.description,
+          decoration: InputDecoration(
+              labelText: LocaleKeys.localization_app_description.tr),
+          validator: controller.validate,
+        ),
+        verticalGap(),
+        Obx(
+          () => controller.selectedImage.value.isEmpty
+              ? const ClipOval(child: Icon(Icons.image_not_supported))
+              : SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.memory(
+                      base64Decode(controller.selectedImage.value)),
+                ),
+        ),
+        verticalGap(),
+        ElevatedButton(
+          onPressed: controller.pickImage,
+          child: Text(LocaleKeys.localization_app_pick_image.tr),
+        ),
+        const SizedBox(height: 16),
+        // Obx(() => TextField(
+        //       controller: TextEditingController(
+        //           text: controller.selectedDate.value),
+        //       readOnly: true,
+        //       decoration: const InputDecoration(labelText: 'Date'),
+        //       onTap: () => controller.selectDate(context),
+        //     )),
+        _date(),
+        horizontalGap(),
+        // Hour Dropdown
+        _time(),
+
+        TextFormField(
+          controller: controller.capacity,
+          decoration: InputDecoration(
+              labelText: LocaleKeys.localization_app_capacity.tr),
+          keyboardType: TextInputType.number,
+          validator: controller.validate,
+          inputFormatters: controller.capacityFormatters,
+        ),
+        TextFormField(
+          controller: controller.price,
+          decoration:
+              InputDecoration(labelText: LocaleKeys.localization_app_price.tr),
+          keyboardType: TextInputType.number,
+          validator: controller.validate,
+          inputFormatters: controller.priceFormatters,
+          onChanged: (value) => int.parse(value),
+        ),
+        verticalGap(),
+        ElevatedButton(
+          onPressed: controller.addEvent,
+          child: Text(LocaleKeys.localization_app_add_event.tr),
+        )
+      ],
+    );
+  }
+
+  Widget _date() {
+    return Row(
+      children: [
+        Obx(
+          () => DropdownButton<int>(
+            hint: Text(LocaleKeys.localization_app_year.tr),
+            value: controller.selectedYear.value == 0
+                ? null
+                : controller.selectedYear.value,
+            items: controller.years
+                .map<DropdownMenuItem<int>>(
+                    (int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              controller.selectedYear.value = value!;
+            },
+          ),
+        ),
+        horizontalGap(),
+        // Month Dropdown
+        Obx(
+          () => DropdownButton<int>(
+            hint: Text(LocaleKeys.localization_app_month.tr),
+            value: controller.selectedMonth.value == 0
+                ? null
+                : controller.selectedMonth.value,
+            items: controller.months
+                .map<DropdownMenuItem<int>>(
+                    (int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              controller.selectedMonth.value = value!;
+              controller.selectedDay.value = 0;
+            },
+          ),
+        ),
+        horizontalGap(),
+        // Day Dropdown
+        Obx(
+          () => DropdownButton<int>(
+            hint: Text(LocaleKeys.localization_app_day.tr),
+            value: controller.selectedDay.value == 0
+                ? null
+                : controller.selectedDay.value,
+            items: controller.days
+                .map<DropdownMenuItem<int>>(
+                    (int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              controller.selectedDay.value = value!;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _time() {
+    return Row(
+      children: [
+        Obx(
+          () => DropdownButton<int>(
+            hint: Text(LocaleKeys.localization_app_hour.tr),
+            value: controller.selectedHour.value == 0
+                ? null
+                : controller.selectedHour.value,
+            items: controller.hours
+                .map<DropdownMenuItem<int>>(
+                    (int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              controller.selectedHour.value = value!;
+            },
+          ),
+        ),
+        horizontalGap(),
+        // Minute Dropdown
+        Obx(
+          () => DropdownButton<int>(
+            hint: Text(LocaleKeys.localization_app_minute.tr),
+            value: controller.selectedMinute.value == 0
+                ? null
+                : controller.selectedMinute.value,
+            items: controller.minutes
+                .map<DropdownMenuItem<int>>(
+                    (int value) => DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(value.toString()),
+                        ))
+                .toList(),
+            onChanged: (value) {
+              controller.selectedMinute.value = value!;
+            },
+          ),
+        ),
+      ],
+    );
+  }
 }
